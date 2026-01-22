ccc2a22c99c0f89f1ebb28f3168885e37bb729c5
rishitasen828-svg <rishitasen828@gmail.com>
2026-01-22 20:05:00 +0530

    Create Numpy_T1.py

---
+++ Numpy_T1.py
@@ -0,0 +1,10 @@
+import numpy as np
+marks=np.array([40,48,50,52,60])
+print("The Marks of the students are:")
+print(marks)
+updated_marks=marks+5
+print("The Updated marks of the students are:")
+print(updated_marks)
+print("The Mean score of the class is:")
+mean=sum(marks)/len(marks)
+print(mean)
