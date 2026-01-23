1f3d5c07cf2d90e268dfb18dff3929076d5cbc54
rishitasen828-svg <rishitasen828@gmail.com>
2026-01-23 10:01:51 +0530

    Update Numpy_T1.py

--- Numpy_T1.py
+++ Numpy_T1.py
@@ -1,10 +1,11 @@
 import numpy as np
-marks=np.array([40,48,50,52,60])
+marks=np.array([40,44,50,52,60])
 print("The Marks of the students are:")
 print(marks)
-updated_marks=marks+5
+marks[marks<50]=marks[marks<50]+5
 print("The Updated marks of the students are:")
-print(updated_marks)
+print(marks)
 print("The Mean score of the class is:")
 mean=sum(marks)/len(marks)
 print(mean)
+
